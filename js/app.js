4eb11641a22f7e6ac26e274300bc4c7fcf9e89bf
emad-hash <emadhashash76@gmail.com>
2023-08-18 19:37:40 +0300

    section 2 done but res and active or center not ready

--- js/app.js
+++ js/app.js
@@ -17,4 +17,38 @@ let typed = new Typed("#auto-input", {
     backspeed:200,
     loop: true
 })
-  
+
+let owl = $(".img-carousel");
+        owl.owlCarousel({
+            // margin: 15,
+            // loop: false,
+            stagePadding: 10,
+            nav: true,
+            navText: ["<b>‹</b>", "<b>›</b>"],
+            merge: true,
+            // left: true,
+            dotsEach: true,
+            autoplayHoverPause: true,
+            animateOut: 'flipInY',
+            animateIn: 'flipInY',
+            autoplay: false,
+            rewind: true,
+            autoplayTimeout: 10000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+
+                900: {
+                    items: 3
+                }
+            }
+        });
+        owl.on('mousewheel', '.owl-stage', function(e) {
+            if (e.deltaY > 0) {
+                owl.trigger('next.owl');
+            } else {
+                owl.trigger('prev.owl');
+            }
+            e.preventDefault();
+        });
