76e5fdec88b447041b87e2284301bb6880df0d9d
emad-hash <emadhashash76@gmail.com>
2023-08-20 00:42:14 +0300

    section 2 and 3 and 4 and 5 done but res and active or center section2 not ready

--- js/app.js
+++ js/app.js
@@ -52,3 +52,72 @@ let owl = $(".img-carousel");
             }
             e.preventDefault();
         });
+
+
+        // ! later
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".cardwrapper").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper2 i");
+const carouselChildrens = [...carousel.children];
+
+// get the number of cards that can fit in the arousel at once
+// let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+// insert copies of the last few cards to beginning od carousel for infinite scrolling
+// carouselChildrens.slice(-cardPerView).reverse().forEach(cardwrapper => {
+//     carousel.insertAdjacentHTML("afterbegin", cardwrapper.outerHTML);
+// });
+// insert copies of the last few cards to beginning od carousel for infinite scrolling
+// carouselChildrens.slice(0, cardPerView).forEach(cardwrapper => {
+//     carousel.insertAdjacentHTML("beforeend", cardwrapper.outerHTML);
+// });
+
+// ? add eventlistener for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // console.log(btn.id);
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
+// const autoPlay = () => {
+//     if(window.innerWidth < 992) return ; // return id window is smaller than 800
+//     // autopaly the carousel is at the beginning, scroll to the end 
+//     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 500)
+// }
+// autoPlay();
+
+
+
+
+
+
+
+
+
+
+
+
+// const infiniteScroll = () => {
+//     // if the carousel is at the beginning, scroll to the end
+//     if(carousel.scrollLeft === 0) {
+//         // console.log("left end");
+//         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+//     }else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+//         // console.log("right end");
+//         carousel.scrollLeft = carousel.scrollWidth - carousel.offsetWidth;
+
+//     } 
+//    }
+//     carousel.addEventListener("scroll", infiniteScroll)
+        // let isDragging = false;
+        
+        // const dragStart = () => {
+        //     isDragging = true;
+        // }
+        
+        // const dragging = (e) => {
+        //     if(!isDragging) return; // is isDraggung is false return from here
+        //     carousl.scrollLeft = e.pageX;
+        // }
+        
+        // carousl.addEventListener("mousemove", dragging);
+        
